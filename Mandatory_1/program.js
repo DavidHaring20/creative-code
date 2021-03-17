e823ea01b80889c302f6399434184f1c25998ce6
David <davidharingri@gmail.com>
2021-03-17 14:49:35 +0100

    Set up of the Canvas, initiated some modules for Matter.js, made constants for color scheme.

--- Mandatory_1/program.js
+++ Mandatory_1/program.js
@@ -1,5 +1,21 @@
+// Set up the Canvas
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight * 0.80;
+
+// Constants
+const backgroundColor = 'rgb(69, 61, 66)';
+const particleColor = 'rgb(235, 16, 153)';
+const particleWidth = 5;
+
+// Background
+context.beginPath()
+context.fillStyle = backgroundColor;
+context.rect(0,0,canvas.width, canvas.height);
+context.stroke();
+context.fill();
 
 // Modules 
 let Engine = Matter.Engine;
@@ -12,8 +28,6 @@ let engine = Engine.create();
 
 // Create a Render 
 let render = Render.create({
-    element: document.body,
+    element: document.body.canvas,
     engine: engine
 });
-
-let background
