1b2cebcc07f7f3a2b89860d731e2889204018197
David <davidharingri@gmail.com>
2021-03-19 13:18:02 +0100

    Managed to move objects using MouseConstraing and Mouse.

--- Mandatory_1/program.js
+++ Mandatory_1/program.js
@@ -4,17 +4,14 @@ const particleColor = 'rgb(235, 16, 153)';
 const floorColor = 'rgba(255, 255, 255, 0.6)';
 const particleWidth = 5;
 
-const width = document.innerWidth;
-const height = document.innerHeight;
-
-console.log(width, height);
-
 // Modules 
 const Engine = Matter.Engine;
 const Bodies = Matter.Bodies;
 const Render = Matter.Render;
 const World = Matter.World;
 const Composite = Matter.Composite;
+const Mouse = Matter.Mouse;
+const MouseConstraint = Matter.MouseConstraint;
 
 // Create an Engine
 const engine = Engine.create();
@@ -38,7 +35,6 @@ const floor = Bodies.rectangle(0, 650, 3000, 4, {
     }
 });
 
-
 let box1 = Bodies.rectangle(400, 40, 150, 150);
 let box2 = Bodies.rectangle(450, 630, 150, 150);
 let box3 = Bodies.rectangle(220, 180, 150, 150);
@@ -46,8 +42,14 @@ let box4 = Bodies.rectangle(520, 110, 150, 150);
 let box5 = Bodies.rectangle(370, 250, 150, 150);
 let box6 = Bodies.rectangle(1000, 100, 150, 150);
 
+let bodyMouse = Mouse.create(document.body.canvas);
+let mouseOptions = {
+    mouse: bodyMouse
+}
+let mouseConstraint = MouseConstraint.create(engine, mouseOptions)
+
 // Add everything to the world
-World.add(engine.world, [floor, box1, box2, box3, box4, box5, box6]);
+World.add(engine.world, [mouseConstraint, floor, box1, box2, box3, box4, box5, box6]);
 
 // Run an Engine
 Engine.run(engine)
